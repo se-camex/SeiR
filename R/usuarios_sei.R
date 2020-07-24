97b350b0de35cbba17b87b54e86a8297cfa64dcb
se-camex <64045019+se-camex@users.noreply.github.com>
2020-07-24 15:16:50 -0300

    Create usuarios_sei.R
    script para coletar os usuários SEI das caixas que o usuário tem acesso.

---
+++ R/usuarios_sei.R
@@ -0,0 +1,96 @@
+library(RSelenium)
+library(dplyr)
+library(purrr)
+get_user_sei_unidade <- function(i) {
+  cat(i%>%paste0("\n"))
+  unidades <- remDr$findElement(value="selInfraUnidades", using='id')
+  unidades$clickElement()
+  m <- unidades$findChildElement(value=paste0("option[value=\"", i, "\""), using='css')
+  m$clickElement()
+  processo <- remDr$findElement(value="chkRecebidosItem0", using='id')
+  processo$clickElement()
+  atribuir <- remDr$findElement(value="#divComandos :nth-child(3) .infraCorBarraSistema", using='css')
+  atribuir$clickElement()
+  users <- remDr$getPageSource()%>%as.character()%>%get_users_sei()
+  users
+}
+
+get_users_sei <- function(x) {
+  require(rvest)
+  x <- read_html(x)
+  unidades <- x%>%html_node("#selInfraUnidades")%>%html_nodes("option")
+  unidade <- unidades[unidades%>%as.character%>%grep("selected", .)]%>%html_text
+  usuarios <- x%>%html_node("#selAtribuicao")%>%html_nodes("option")
+  values <- usuarios%>%html_attr("value")#%>%unlist
+  values <- values[!values%in%"null"]
+  names <- usuarios%>%
+    html_text%>%
+    strsplit(" - |class")%>%
+    purrr::map_dfr(function(x) tibble(user_sei=x[1], nome_sei=x[2])%>%
+                     filter(!is.na(nome_sei)))%>%
+    arrange(nome)%>%mutate(unidade=unidade, id_sei=values, user_sei)
+  names
+}
+
+
+# O chrome bloqueia o acesso ao SEI por conta de certificado.
+# Ainda não solucionei o problema.
+#
+# cprof <- getChromeProfile(
+#   #"C:\\Users\\eleon\\AppData\\Local\\Google\\Chrome\\User Data",
+#   "/home/seluser/.config/google-chrome",
+#   "Profile 1"
+# )
+#
+#
+# cprof$chromeOptions$args <- list("--allow-running-insecure-content", "--ignore-ssl-errors=yes", "--ignore-certificate-errors", "--disable-web-security", "--allow-insecure-localhost", "--unsafely-treat-insecure-origin-as-secure")
+
+## Utilizando o Docker (local) do Selenium
+
+remDr<-
+  remoteDriver(remoteServerAddr = "192.168.99.100"
+               , port = 4445L
+               , browserName = "chrome"
+              #, extraCapabilities = list(
+               # args=cprof#"--allow-running-insecure-content"
+                # args="--ignore-certificate-errors"
+                # "ignore-ssl-errors"="yes",
+                #                          "accept_insecure_certs"=TRUE
+                                         #chrome.switches="--ignore-certificate-errors")
+                                         )
+
+
+remDr$closeall()
+remDr$open()
+remDr$navigate("https://sei.fazenda.gov.br")
+
+
+##login
+user <- remDr$findElement(value = "txtUsuario", using = "id")
+user$clearElement()
+user$sendKeysToElement(list(user_sei))
+pwd <- remDr$findElement(value = "pwdSenha", using = "id")
+pwd$clearElement()
+pwd$sendKeysToElement(list(pwd_sei))
+login <- remDr$findElement(value="sbmLogin", using='id')
+login$clickElement()
+
+
+unidades <- remDr$findElement(value="selInfraUnidades", using='id')
+unidades_sel <- unidades$selectTag()
+unidades_disp <- unidades_sel$text
+
+library(tictoc)
+tic()
+usuarios <- map_dfr(unidades_sel$value, possibly(get_user_sei_unidade, otherwise = tibble()))
+toc()
+
+unidades_missing <-(tibble(unidade=unidades_disp)%>%anti_join(usuarios))
+stopifnot(nrow(unidades_missing%>%nrow)>0)
+
+remDr$closeall()
+remDr$closeServer()
+
+
+#usuarios <- usuarios%>%rename(user_sei=id_sei, id_sei=id_sei_num, nome_sei=nome)
+
